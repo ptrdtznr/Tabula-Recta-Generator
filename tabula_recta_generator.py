1a1b0c0b9e744569775bfd1aeb0dfa7cd54f4f38
Peter Detzner <peter.detzner@iml.fraunhofer.de>
2020-09-13 20:13:01 +0200

    Initial integration of pw generator

---
+++ tabula_recta_generator.py
@@ -0,0 +1,34 @@
+# this password generator is based on https://lifehacker.com/how-to-write-down-and-encrypt-your-passwords-with-an-ol-5715794
+import random
+import string
+
+
+CHAR_SET = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()+_-{}|;<>?ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+SPACING = " | "
+
+
+def get_random_char():
+    pos = random.randint(0, len(CHAR_SET)-1)
+    return CHAR_SET[pos]
+
+
+def tabula_recta_generator():
+
+    alphabet = list(string.ascii_lowercase)
+
+    for x in range(0, len(alphabet)):
+        print(alphabet[x],  end=SPACING)
+    print()
+
+    for row in range(0, len(alphabet)):
+        print(alphabet[row],  end=SPACING)
+        for column in range(0, len(alphabet)-1):
+            print(get_random_char(), end=SPACING)
+        print("")
+
+
+if __name__ == '__main__':
+    personal_seed = input("Set your private seed (or leave blank for random): \n")
+    if personal_seed:
+        random.seed(personal_seed)
+    tabula_recta_generator()
